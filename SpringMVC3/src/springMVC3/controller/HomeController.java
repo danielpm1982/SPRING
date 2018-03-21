b19fe936be80daeeb432af96516ec722be7af557
Daniel Pinheiro <danielpm1982@gmail.com>
2018-03-21 08:55:57 -0300

    .

---
+++ SpringMVC3/src/springMVC3/controller/HomeController.java
@@ -0,0 +1,11 @@
+package springMVC3.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String showHomePage() {
+		return "home";
+	}
+}
