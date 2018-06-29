2959d7ec3523de8c22476963ca77ea9f7d341dbb
Daniel Pinheiro <danielpm1982@gmail.com>
2018-06-28 22:54:30 -0300

    .

---
+++ SpringMVC3JPA2/src/springMVC3/dao/OrderDAOInterface.java
@@ -0,0 +1,12 @@
+package springMVC3.dao;
+import java.util.List;
+
+import springMVC3.entity.Order;
+
+public interface OrderDAOInterface {
+	public Long saveOrder(Order mockOrder);
+	public Order getOrderById(Long orderId);
+	public List<Order> getOrderBySSN(Long SSN);
+	public List<Order> getAllOrders();
+	public boolean deleteOrder(Long orderId);
+}
---
+++ SpringMVC3JPA2/src/springMVC3/helper/SandwichType.java
@@ -0,0 +1,5 @@
+package springMVC3.helper;
+
+public enum SandwichType {
+	BIGMEAL, CHEESEMEAL, CHICKENMEAL, BBQMEAL, FISHMEAL;
+}
